ab161c954b8f4d3ff1b944d8091d8511afeb163d
Mykola <Nikolaua36@gmail.com>
2026-03-01 01:23:48 +0200

    Install phpunit for Symfony

---
+++ tests/Unit/HelloWorldTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class HelloWorldTest extends TestCase
+{
+    public function testSomething(): void
+    {
+        $this->assertTrue(true);
+    }
+}
