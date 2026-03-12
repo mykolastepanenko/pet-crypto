09b089ade4e9c9eaf49a9ee3b4471f09bc1f0897
Mykola <Nikolaua36@gmail.com>
2026-03-12 17:49:37 +0200

    Install libs: data-fixtures and faker and factory

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,6 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
 ];
