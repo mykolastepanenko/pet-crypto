2076b55d8d35e548add88bbd1a7663d82637adb6
Ivan Bazyuk <bazyuk2001@gmail.com>
2026-03-21 15:23:05 +0200

    Update IvanMessage

--- src/MessageHandler/IvanMessageHandler.php
+++ src/MessageHandler/IvanMessageHandler.php
@@ -3,18 +3,20 @@
 namespace App\MessageHandler;
 
 use App\Message\IvanMessage;
-use App\Service\Message\IvanMessageLoggerInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
 #[AsMessageHandler]
-final class IvanMessageHandler
+class IvanMessageHandler
 {
     public function __construct(
-        private readonly IvanMessageLoggerInterface $logger
+        private readonly LoggerInterface $logger
     ) {}
 
     public function __invoke(IvanMessage $message): void
     {
-        $this->logger->log($message->getText());
+        $this->logger->info('IvanMessage processed', [
+            'message' => $message->getText()
+        ]);
     }
 }
--- src/Service/Message/IvanMessageLoggerInterface.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace App\Service\Message;
-
-interface IvanMessageLoggerInterface
-{
-    public function log(string $message): void;
-}
